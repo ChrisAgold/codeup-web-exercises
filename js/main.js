c32c7b956e73b664504025e1d28530aac12065a2
Chris Agold <christophersagold@gmail.comm>
2020-11-05 16:53:13 -0600

    Becoming a Javascript Wizard

---
+++ js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("Hello, from the Javascript console!");
+
+// alert("You just won!");
+// alert("Just kidding.");
+//
+// var iPodWinner = "You just won a iPod!";
+// alert(iPodWinner);
+
+// the following line will show the OK/CANCEL confirm dialog
+// var confirmed = confirm('Are you sure you want to do XYZ?');
+// console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
+
