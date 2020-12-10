c7a54c80cb01d589f6d35fa630a02d2ce5375ee2
ChrisAgold <christophersagold@gmail.com>
2020-12-10 16:44:27 -0600

    deleted keys.js

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-// This file is for saving API Keys & not uploading to Github //
-let mapboxToken = 'pk.eyJ1IjoiemVsZGExMTY2IiwiYSI6ImNraWpmZmQyNzAyNzIyeXBjcnduM3UyZHcifQ.5ySRyc2GPgysRlMgYT-7gg';
-
-
-
